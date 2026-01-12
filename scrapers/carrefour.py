df3758796ee74863fabc6402cfc985dae0144459
Ksiwar1 <118802909+Ksiwar1@users.noreply.github.com>
2026-01-12 16:11:55 +0100

    Implement category scraping function for Carrefour
    Added a new function to scrape product details for a specific category and store.

--- scrapers/carrefour.py
+++ scrapers/carrefour.py
@@ -12,7 +12,7 @@ import random
 # =========================
 
 CATEGORIES = {
-    "pates": "https://www.carrefour.fr/r/epicerie-salee/pates"
+    "beurre": "https://www.carrefour.fr/s?q=beurre++"
 }
 
 OUTPUT_CSV = "carrefour_prix_par_magasin.csv"
@@ -104,11 +104,39 @@ def scrape_category_for_store(driver, category_url, store, category_name):
     soup = BeautifulSoup(driver.page_source, "html.parser")
     produits = []
 
+   def scrape_category_for_store(driver, category_url, store, category_name):
+    print(f"  ↳ Catégorie : {category_name}")
+    driver.get(category_url)
+    time.sleep(random.uniform(4, 6))
+
+    soup = BeautifulSoup(driver.page_source, "html.parser")
+    produits = []
+
     cards = soup.find_all("a", class_="product-card-click-wrapper")
 
     for card in cards:
-        nom = card.find("h3", class_="product-card-title__text")
-        prix = card.find("span", class_="product-price__content")
+        # NOM DU PRODUIT
+        nom = card.find(
+            "p",
+            class_="product-card-title__text"
+        )
+
+        # PRIX PRINCIPAL
+        prix = card.find(
+            "span",
+            class_="product-price__amount product-price__amount--main"
+        )
+
+        produits.append({
+            "produit": nom.text.strip() if nom else "N/A",
+            "prix": prix.text.strip() if prix else "N/A",
+            "categorie": category_name,
+            "magasin": store["nom"],
+            "url_magasin": store["url"]
+        })
+
+    print(f"    → {len(produits)} produits récupérés")
+    return produits
 
         produits.append({
             "produit": nom.text.strip() if nom else "N/A",
